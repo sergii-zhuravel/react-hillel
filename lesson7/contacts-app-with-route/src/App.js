cd364cb31cf0aa7203ce924176d3230ffe692c3c
Serhii Zhuravel <szhuravell@gmail.com>
2022-08-02 16:19:15 +0300

    updated react router

--- lesson7/contacts-app-with-route/src/App.js
+++ lesson7/contacts-app-with-route/src/App.js
@@ -1,6 +1,6 @@
 import Contacts from "./components/contacts/Contacts";
 import ThemeProvider from "./context/ThemeProvider";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
@@ -10,20 +10,12 @@ function App() {
           <Link to={"/contacts"}>Contacts</Link> |{" "}
           <Link to={"/users"}>Users</Link> | <Link to={"/posts"}>Posts</Link>
         </div>
-        <Switch>
-          <Route path={"/contacts"} exect>
-            <Contacts />
-          </Route>
-          <Route path={"/contacts/:id"}>
-            <div>Edit contact</div>
-          </Route>
-          <Route path={"/users"}>
-            <div>Users</div>
-          </Route>
-          <Route path={"/posts"}>
-            <div>Posts</div>
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="contacts" element={<Contacts />} />
+          <Route path="contacts/:id" element={<div>Edit contact</div>} />
+          <Route path="users" element={<div>Users</div>} />
+          <Route path="posts" element={<div>Posts</div>} />
+        </Routes>
       </BrowserRouter>
     </ThemeProvider>
   );
